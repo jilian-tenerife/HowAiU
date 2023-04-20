9b2bcc248259f34f96efc4abff4fd886a9e0e108
Jillian <jillian.tenerife@usjr.edu.ph>
2023-04-21 03:05:59 +0800

    go jerry

--- howaiu/lib/screens/chat.dart
+++ howaiu/lib/screens/chat.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 import '../widgets/chat_bubble.dart';
 
@@ -14,7 +15,7 @@ class _ChatAiuState extends State<ChatAiu> {
   final TextEditingController _controller = TextEditingController();
   List<ChatBubble> _messages = [
     const ChatBubble(
-      text: 'How can I help you bitch lasagna?',
+      text: 'How can I help you?',
       isCurrentUser: false,
     ),
   ];
@@ -31,10 +32,28 @@ class _ChatAiuState extends State<ChatAiu> {
 
   @override
   Widget build(BuildContext context) {
+    Color baseColor = const Color(0xffdadada);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chat AIU'),
-        backgroundColor: Colors.grey[300],
+      backgroundColor: Color(0xffabb6c8),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(56.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(16.0)),
+          child: Neumorphic(
+            style: NeumorphicStyle(
+              depth: 10,
+              shape: NeumorphicShape.concave,
+              lightSource: LightSource.bottomLeft,
+              color: baseColor,
+            ),
+            child: AppBar(
+              backgroundColor: Color(0xffabb6c8),
+              elevation: 10,
+              title: const Text('AiU'),
+            ),
+          ),
+        ),
       ),
       body: Column(
         children: [
@@ -48,23 +67,38 @@ class _ChatAiuState extends State<ChatAiu> {
             ),
           ),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
             child: Row(
               children: [
                 Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintText: 'Type your message...',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0),
+                  child: Neumorphic(
+                    style: NeumorphicStyle(
+                      depth: 4,
+                      shape: NeumorphicShape.convex,
+                      lightSource: LightSource.topLeft,
+                      color: Color(0xffabb6c8),
+                    ),
+                    child: TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        hintText: 'Type your message...',
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 15.0, vertical: 12.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
                       ),
                     ),
                   ),
                 ),
-                IconButton(
+                NeumorphicButton(
                   onPressed: _sendMessage,
-                  icon: const Icon(Icons.send),
+                  child: Icon(
+                    Icons.send,
+                    color: Color(0xffabb6c8),
+                  ),
                 ),
               ],
             ),
